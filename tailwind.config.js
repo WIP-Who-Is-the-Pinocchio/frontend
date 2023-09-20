86bc5a5b072f7c2f97959a330b09a0b0eb174c41
gomao <gboycdw@gmail.com>
2023-09-20 12:17:33 +0900

    chore: tailwind config 설정 및 테스트 완료

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import { tailwindStyle } from "./src/styles/tailwindStyle";
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: { ...tailwindStyle },
   },
   plugins: [],
 };
