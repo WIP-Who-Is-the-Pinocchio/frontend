e7886f7ad5f58832f45864703c522486327cb8e7
Yoonjae Choi <dbswo9795@gmail.com>
2023-09-24 00:06:09 +0900

    fix: 테이블 중앙 정렬 및 UI 수정

--- src/pages/Admin/AdminSearch.tsx
+++ src/pages/Admin/AdminSearch.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
 import Dropdown from "@components/Dropdown";
-import { memberData } from "@assets/datas/memberData";
+import { memberData, MemberType } from "@assets/datas/memberData";
 
 const AdminSearch = () => {
   const [searchValue, setSearchValue] = useState<string>("");
   const [selectedItem, setSelectedItem] = useState<string>("이름");
+  const [displayData, setDisplayData] = useState<MemberType[]>([]);
 
   const searchValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const currentValue = e.target.value;
@@ -14,15 +15,19 @@ const AdminSearch = () => {
 
   //드롭다운으로 필터 후 검색 데이터 보여줌
   const searchData = (selectedItem: string) => {
-    const filteredData = memberData.filter((member) => {
+    return memberData.filter((member) => {
       const value = member[selectedItem];
       if (typeof value === "string") {
         return value.includes(searchValue);
       }
-      return "";
+      return false;
     });
+  };
 
-    return filteredData;
+  // 검색 버튼 클릭했을 떄, 데이터 변하도록
+  const searchButtonClicked = () => {
+    const filteredData = searchData(selectedItem);
+    setDisplayData(filteredData);
   };
 
   return (
@@ -40,29 +45,32 @@ const AdminSearch = () => {
             onChange={searchValueChange}
             className="border border-black rounded w-96 h-7 mr-10 p-1"
           />
-          <button className="border border-sk w-20 h-8 rounded-lg bg-purple-100">
+          <button
+            onClick={searchButtonClicked}
+            className="border border-sk w-20 h-8 rounded-lg bg-purple-100"
+          >
             검색
           </button>
         </div>
       </div>
       <div className="bg-white w-full min-h-3/4 rounded-lg p-10">
         <div className="flex justify-between border-b p-5">
-          <div>이름</div>
-          <div>당선횟수</div>
-          <div>소속정당</div>
-          <div>지역구</div>
-          <div>총 공약수</div>
-          <div>완료</div>
-          <div>추진중</div>
-          <div>필요재정총액</div>
-          <div>확보재정총액</div>
-          <div>집행재정총액</div>
+          <div className="w-1/16 text-center">이름</div>
+          <div className="w-1/16 text-center">당선횟수</div>
+          <div className="w-2/16 text-center">소속정당</div>
+          <div className="w-3/16 text-center">지역구</div>
+          <div className="w-1/16 text-center">총 공약수</div>
+          <div className="w-1/16 text-center">완료</div>
+          <div className="w-1/16 text-center">추진중</div>
+          <div className="w-2/16 text-center">필요재정총액</div>
+          <div className="w-2/16 text-center">확보재정총액</div>
+          <div className="w-2/16 text-center">집행재정총액</div>
         </div>
-        {searchValue ? (
+        {displayData.length !== 0 ? (
           <div>
-            {searchData(selectedItem).map((data) => (
-              <div className="flex justify-between border-b p-5">
-                <div className="flex flex-col items-center ">
+            {displayData.map((data) => (
+              <div className="flex justify-between text-center border-b p-5">
+                <div className="flex flex-col text-center items-center w-1/16">
                   <img
                     src={data.프로필}
                     alt={data.이름}
@@ -70,15 +78,15 @@ const AdminSearch = () => {
                   />
                   <div>{data.이름}</div>
                 </div>
-                <div>{data.당선횟수}</div>
-                <div>{data.소속정당}</div>
-                <div>{data.지역구}</div>
-                <div>{data.총공약수}</div>
-                <div>{data.완료}</div>
-                <div>{data.추진중}</div>
-                <div>{data.필요재정총액}</div>
-                <div>{data.확보재정총액}</div>
-                <div>{data.집행재정총액}</div>
+                <div className="w-1/16 text-center">{data.당선횟수}</div>
+                <div className="w-2/16 text-center">{data.소속정당}</div>
+                <div className="w-3/16 text-center">{data.지역구}</div>
+                <div className="w-1/16 text-center">{data.총공약수}</div>
+                <div className="w-1/16 text-center">{data.완료}</div>
+                <div className="w-1/16 text-center">{data.추진중}</div>
+                <div className="w-2/16 text-center">{data.필요재정총액}</div>
+                <div className="w-2/16 text-center">{data.확보재정총액}</div>
+                <div className="w-2/16 text-center">{data.집행재정총액}</div>
               </div>
             ))}
           </div>
@@ -86,7 +94,7 @@ const AdminSearch = () => {
           <div>
             {memberData.map((data) => (
               <div className="flex justify-between border-b p-5">
-                <div className="flex flex-col items-center ">
+                <div className="flex flex-col text-center items-center w-1/16">
                   <img
                     src={data.프로필}
                     alt={data.이름}
@@ -94,15 +102,15 @@ const AdminSearch = () => {
                   />
                   <div>{data.이름}</div>
                 </div>
-                <div>{data.당선횟수}</div>
-                <div>{data.소속정당}</div>
-                <div>{data.지역구}</div>
-                <div>{data.총공약수}</div>
-                <div>{data.완료}</div>
-                <div>{data.추진중}</div>
-                <div>{data.필요재정총액}</div>
-                <div>{data.확보재정총액}</div>
-                <div>{data.집행재정총액}</div>
+                <div className="w-1/16 text-center">{data.당선횟수}</div>
+                <div className="w-2/16 text-center">{data.소속정당}</div>
+                <div className="w-3/16 text-center">{data.지역구}</div>
+                <div className="w-1/16 text-center">{data.총공약수}</div>
+                <div className="w-1/16 text-center">{data.완료}</div>
+                <div className="w-1/16 text-center">{data.추진중}</div>
+                <div className="w-2/16 text-center">{data.필요재정총액}</div>
+                <div className="w-2/16 text-center">{data.확보재정총액}</div>
+                <div className="w-2/16 text-center">{data.집행재정총액}</div>
               </div>
             ))}
           </div>
