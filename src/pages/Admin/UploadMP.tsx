e22d40f0b7b2a816bc8dbc083c16f651934f78d9
bible <mongry999@gmail.com>
2023-09-27 17:43:17 +0900

    fix: 조건부 스타일에 twMerge 사용

--- src/pages/Admin/UploadMP.tsx
+++ src/pages/Admin/UploadMP.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 import FormUploader from "@components/admin/uploadMP/FormUploader";
 import ExcelUploader from "@components/admin/uploadMP/ExcelUploader";
 
@@ -32,21 +33,23 @@ const UploadMP = () => {
         <nav className="text-base font-medium text-center text-gray-500 border-b border-gray-200 ">
           <ul className="flex flex-wrap -mb-px">
             <li
-              className={`px-4 py-3 cursor-pointer ${
+              className={twMerge(
+                "px-4 py-3 cursor-pointer",
                 focusedTab === TabType.Excel
                   ? "border-b-2 border-purple-200 text-purple-500"
-                  : "hover:text-gray-600 hover:border-gray-300"
-              }`}
+                  : "hover:text-gray-600 hover:border-gray-300",
+              )}
               onClick={() => tabChangeHandler(TabType.Excel)}
             >
               업로드
             </li>
             <li
-              className={`px-4 py-3 cursor-pointer ${
+              className={twMerge(
+                "px-4 py-3 cursor-pointer",
                 focusedTab === TabType.Form
                   ? "border-b-2 border-purple-200 text-purple-500"
-                  : "hover:text-gray-600 hover:border-gray-300"
-              }`}
+                  : "hover:text-gray-600 hover:border-gray-300",
+              )}
               onClick={() => tabChangeHandler(TabType.Form)}
             >
               직접 입력
