2d37c197451bce0c814754d10d603f2062912fe2
bible <mongry999@gmail.com>
2023-09-27 19:11:13 +0900

    fix: excel data mapping 테이블 정리

--- src/components/admin/uploadMP/UploadedExcelPreview.tsx
+++ src/components/admin/uploadMP/UploadedExcelPreview.tsx
@@ -50,41 +50,39 @@ const UploadedExcelPreview: React.FC<UploadedExcelPreviewProps> = ({
             </tr>
           </thead>
           <tbody>
-            {excelData && (
-              <>
-                {excelData.map((data, index) => (
-                  <tr key={data["이름"]} className="bg-white border-b">
-                    <th
-                      scope="row"
-                      className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap bg-white sticky left-0 z-10 shadow-[5px_0px_8px_-4px_rgba(0,0,0,.15)]"
-                    >
-                      <div
-                        style={{ backgroundImage: `url(${data[MPDataKeys[0]]})` }}
-                        className={`w-10 h-10 border border-gray-200 rounded-full  bg-cover bg-top`}
-                      />
-                      <div className="pl-3">
-                        <div className="text-[14px] font-semibold">
-                          {data[MPDataKeys[1]]}
-                        </div>
-                      </div>
-                    </th>
-                    {MPDataKeys.slice(2).map((key) => (
-                      <td key={key} className="text-[12px] px-6 py-4 whitespace-nowrap">
-                        {data[key]}
-                      </td>
-                    ))}
-                    <td className="px-6 py-4 bg-white whitespace-nowrap sticky right-0 z-10">
-                      <button
-                        className="bg-stone-100 border border-red-400 text-[12px] text-red-500 rounded py-1 px-2"
-                        onClick={() => handleClickDeleteButton(index)}
-                      >
-                        삭제
-                      </button>
+            {excelData.map((data, index) => {
+              const profile = data[MPDataKeys[0]];
+              const name = data[MPDataKeys[1]];
+              return (
+                <tr key={name + index} className="bg-white border-b">
+                  <th
+                    scope="row"
+                    className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap bg-white sticky left-0 z-10 shadow-[5px_0px_8px_-4px_rgba(0,0,0,.15)]"
+                  >
+                    <div
+                      style={{ backgroundImage: `url(${profile})` }}
+                      className={`w-10 h-10 border border-gray-200 rounded-full  bg-cover bg-top`}
+                    />
+                    <div className="pl-3">
+                      <div className="text-[14px] font-semibold">{name}</div>
+                    </div>
+                  </th>
+                  {MPDataKeys.slice(2).map((key) => (
+                    <td key={key} className="text-[12px] px-6 py-4 whitespace-nowrap">
+                      {data[key]}
                     </td>
-                  </tr>
-                ))}
-              </>
-            )}
+                  ))}
+                  <td className="px-6 py-4 bg-white whitespace-nowrap sticky right-0 z-10">
+                    <button
+                      className="bg-stone-100 border border-red-400 text-[12px] text-red-500 rounded py-1 px-2"
+                      onClick={() => handleClickDeleteButton(index)}
+                    >
+                      삭제
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
