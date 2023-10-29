fe8b86382b9e2018b2221edfb2dfdf7b357a2dda
bible <mongry999@gmail.com>
2023-10-29 21:46:31 +0900

    feat: 세부지역구 컴포넌트 react hook form 레지스터 등록

--- src/components/admin/uploadMP/formUploader/SelectDistrict.tsx
+++ src/components/admin/uploadMP/formUploader/SelectDistrict.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Title from "./Title";
+import { UseFormRegisterReturn } from "react-hook-form";
 
 interface SelectDistrictProps {
   title: string;
@@ -9,6 +10,7 @@ interface SelectDistrictProps {
   required?: boolean | false;
   tooltip?: string;
   caption?: string;
+  onRegister: UseFormRegisterReturn;
   onSelectDistrict: (index: number, event: React.ChangeEvent<HTMLSelectElement>) => void;
   onClickDeleteCityComponent: (index: number) => void;
 }
@@ -20,6 +22,7 @@ const SelectDistrict: React.FC<SelectDistrictProps> = ({
   index,
   required,
   tooltip,
+  onRegister,
   onSelectDistrict,
   onClickDeleteCityComponent,
 }) => {
@@ -39,6 +42,7 @@ const SelectDistrict: React.FC<SelectDistrictProps> = ({
       <div className={"border border-gray-300 rounded-lg bg-gray-50"}>
         <select
           className="block w-full h-[43px] p-[10px] border-r-[16px] border-transparent rounded-lg text-[12px] text-gray-900 outline-none"
+          {...onRegister}
           onChange={(e) => onSelectDistrict(index, e)}
         >
           <option value={""}>선택해주세요.</option>
@@ -46,7 +50,7 @@ const SelectDistrict: React.FC<SelectDistrictProps> = ({
             <option
               key={optionItem}
               value={optionItem}
-              selected={optionItem === selected}
+              // selected={optionItem === selected}
             >
               {optionItem}
             </option>
