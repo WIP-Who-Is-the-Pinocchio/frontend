03424f75ba6eec73fc839e9e006ab7faf04fb0bb
o2rynn <flsgp0317@gmail.com>
2023-12-03 21:20:09 +0900

    feat: 이메일 인증 및 확인 기능구현

--- src/pages/Admin/AdminSignUp.tsx
+++ src/pages/Admin/AdminSignUp.tsx
@@ -3,18 +3,20 @@ import LoginFormInput from "@components/AdminAuthFormInput";
 import { ErrorMessage } from "@hookform/error-message";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
+import { debounce, set } from "lodash";
 import Button from "@components/Button";
 import logo from "@assets/icon/wipLogo.svg";
 import { createPortal } from "react-dom";
 import Modal from "@components/Modal/Modal";
 import { AdminAuthFormInputs } from "../../types/adminAuthTypes";
 import EmailAuthBtn from "@components/EmailAuthBtn";
-import EmailAuthTimer from "@components/EmailAuthTimer";
+import EmailAuthTimer from "@components/emailTimer";
 
 const AdminSignUp: React.FC = () => {
   const {
     getValues,
     control,
+    setError,
     formState: { errors },
     handleSubmit,
   } = useForm<AdminAuthFormInputs>({
@@ -23,12 +25,24 @@ const AdminSignUp: React.FC = () => {
 
   const [showSignupCompleteModal, setShowSignupCompleteModal] = useState(false);
   const [showEmailAuthModal, setShowEmailAuthModal] = useState(false);
-  const [_, setShouldNavigate] = useState(false);
+  const [nickname, setNickname] = useState("");
+  const [emailValidNum, setEmailValidNum] = useState("");
+  const [isNicknameDuplicated, setIsNicknameDuplicated] = useState(false);
+  const [isEmailValidate, setIsEmailVlidate] = useState(false);
+  const { remainingTime, startTimer, stopTimer, formatTime } = EmailAuthTimer({
+    initialTime: 180,
+  });
+
+  useEffect(() => {
+    // 타이머가 완료되면 모달 닫기
+    if (remainingTime <= 0) {
+      setShowEmailAuthModal(false);
+    }
+  }, [remainingTime]);
 
   const onSubmit = async (data: AdminAuthFormInputs) => {
     console.log(data);
     setShowSignupCompleteModal(true);
-    setShouldNavigate(true);
 
     // const signupRes = await axios.post("http://localhost:2309/admin/api/v1/auth/signup", {
     //   login_name: data.email,
@@ -39,15 +53,97 @@ const AdminSignUp: React.FC = () => {
     // console.log(signupRes);
   };
 
-  const handleTimerComplete = () => {
-    // 타이머가 완료되면 모달 닫기
-    setShowEmailAuthModal(false);
+  const resetTimer = () => {
+    // setIsEmailRequested(true);
+    console.log("재요청됨");
+    stopTimer();
+    startTimer();
   };
 
-  const handleClickEmailAuth = () => {
-    // window.alert("입력하신 이메일 주소로 인증번호가 발송되었습니다.");
+  const handleClickEmailAuth = debounce(() => {
+    if (getValues("email").length == 0) {
+      //이메일 입력값 x
+      alert("이메일을 입력해주세요");
+      return;
+    }
+
+    if (showEmailAuthModal) {
+      //이메일 인증 재요청시
+      resetTimer();
+      axios.post(
+        `http://localhost:2309/admin/api/v1/auth/email/authorization/${getValues(
+          "email",
+        )}`,
+        { email: getValues("email") },
+      );
+    }
+    //처음요청
+    startTimer();
     setShowEmailAuthModal(true);
+
+    alert("이메일 인증요청이 발송되었습니다");
+    axios.post(
+      `http://localhost:2309/admin/api/v1/auth/email/authorization/${getValues("email")}`,
+      { email: getValues("email") },
+    );
+    console.log(getValues("email"));
+  }, 300);
+
+  const handleClickConfirmEmail = async () => {
+    //이메일 인증메일 값 일치확인
+    await axios
+      .get(
+        `http://localhost:2309/admin/api/v1/auth/email/verification?email=${getValues(
+          "email",
+        )}&auth_number=${emailValidNum}`,
+      )
+      .then(() => {
+        window.alert("이메일 인증이 완료되었습니다");
+        console.log("ddd");
+        setIsEmailVlidate(true);
+      })
+      .catch((e) => {
+        console.log(e);
+        console.log("틀림");
+        window.alert("이메일 인증번호가 일치하지않습니다");
+      });
   };
+
+  const handleChangeAuthEmailNum = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmailValidNum(e.target.value);
+  };
+
+  const handleChangeNickNameDuplication = debounce((e) => {
+    const nickname = e.target.value;
+    setNickname(nickname);
+    console.log(nickname);
+    console.log("get" + nickname);
+    axios
+      .get(`http://localhost:2309/admin/api/v1/auth/nickname/${nickname}`)
+      .then((response) => {
+        console.log(response.data.detail);
+        console.log(response.status);
+        if (response.data.detail === "Nickname already exists.") {
+          setIsNicknameDuplicated(true);
+          //hookform 에러 설정
+          if (isNicknameDuplicated) {
+            console.log("중복");
+            setError("nickname", {
+              type: "duplicated",
+              message: "닉네임이 중복되었습니다.",
+            });
+          } else {
+            setError("nickname", {
+              type: "duplicated",
+              message: "",
+            });
+          }
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, 300);
 
   return (
     <div className="flex justify-center items-center min-h-[100vh] ">
@@ -91,26 +187,32 @@ const AdminSignUp: React.FC = () => {
                     className="w-[200px]"
                   />
                   <div className="translate-y-[10px]">
-                    <EmailAuthBtn type="button" onClick={handleClickEmailAuth} />
+                    <EmailAuthBtn type="button" onClick={handleClickEmailAuth}>
+                      {showEmailAuthModal ? "재요청" : "인증하기"}
+                    </EmailAuthBtn>
                   </div>
                 </div>
                 {showEmailAuthModal && (
-                  <div className="flex flex-col justify-center items-center w-[200px] p-[10px] text-[10px] text-center ">
+                  <div className="w-[100%] text-[10px] text-center ">
                     <div className="flex items-center">
                       <input
                         type="text"
                         placeholder="인증번호를 입력해주세요"
-                        className="m-[15px] p-[10px] border rounded"
+                        className="w-[95%] m-[10px] p-[8px] border rounded"
+                        onChange={handleChangeAuthEmailNum}
                       />
-                      <div className="w-[50px] h-[30px] p-[10px] bg-slate-200">
+                      <button
+                        className="w-[50px] h-[25px] p-[5px] bg-violet-400 text-white font-semibold rounded"
+                        onClick={handleClickConfirmEmail}
+                      >
+                        확인
+                      </button>
+                      <div className="w-[50px] h-[30px] flex items-center p-2">
                         {showEmailAuthModal && (
-                          <EmailAuthTimer onComplete={handleTimerComplete} />
+                          <div className="text-red-400">{formatTime(remainingTime)}</div>
                         )}
                       </div>
                     </div>
-                    <button className="w-[50px] h-[25px] p-[5px] bg-violet-400 text-white font-semibold rounded ">
-                      확인
-                    </button>
                   </div>
                 )}
               </div>
@@ -147,6 +249,7 @@ const AdminSignUp: React.FC = () => {
                   id="nickname"
                   type="text"
                   placeholder="닉네임을 입력해주세요"
+                  onChange={handleChangeNickNameDuplication}
                 />
               </div>
             )}
