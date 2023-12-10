5e7cff94f66a36debf15b5fa5fac53182e65ebce
Yoonjae Choi <dbswo9795@gmail.com>
2023-12-10 22:25:09 +0900

    fix: 데이터 문구 수정

--- src/components/CheckDuplicated/CheckDuplicated.tsx
+++ src/components/CheckDuplicated/CheckDuplicated.tsx
@@ -8,8 +8,8 @@ const CheckDuplicatedUpload = () => {
     <div className="bg-purple-50 h-36 p-5 rounded-md overflow-y-scroll drop-shadow-md">
       {integrityData?.duplicated_jurisdiction.map((data: DuplicatedDataType) => (
         <div className="pb-1">
-          `${data.region} ${data.district} ${data.section}`의{" "}
-          {data.politician_list[0].name}의 데이터가 중복 업로드 되었습니다.
+          `${data.region} ${data.district} ${data.section}` {data.politician_list[0].name}
+          의 데이터가 중복 업로드 되었습니다.
         </div>
       ))}
     </div>
