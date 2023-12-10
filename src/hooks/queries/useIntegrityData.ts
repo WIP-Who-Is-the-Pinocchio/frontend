df4d0bc9497d6201ab7a27bd64c1962ab91928e4
Yoonjae Choi <dbswo9795@gmail.com>
2023-12-10 12:36:49 +0900

    feat: 중복 및 에러 데이터 쿼리 훅 구현

---
+++ src/hooks/queries/useIntegrityData.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { IntegrityDataType } from "../../types/admin.type";
+import { get } from "@api/instance";
+
+const useIntegrityData = () => {
+  const { data: integrityData } = useQuery<IntegrityDataType>(
+    ["integrityData"],
+    async () => {
+      const response = await get(`/integrity-error`);
+      return response.data;
+    },
+  );
+
+  return { integrityData };
+};
+export default useIntegrityData;
