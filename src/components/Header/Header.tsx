1f21e101cedf26b89ce7a603b353c4f045dac4a2
Yoonjae Choi <dbswo9795@gmail.com>
2023-12-10 22:25:23 +0900

    feat: 헤더 모달 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,41 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import HeaderModal from "@components/Modal/HeaderModal";
 import { HamburgerIcon } from "@assets/icon";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [modalOpen, setModalOpen] = useState(false);
 
   const token = localStorage.getItem("accessToken");
 
   return (
-    <div className="flex justify-between w-full px-10 py-5">
-      <img src={HamburgerIcon} alt="hamburger" className="w-10 cursor-pointer" />
-      {token ? (
-        <button
-          className=""
-          onClick={() => {
-            localStorage.removeItem("accessToken");
-            localStorage.removeItem("refreshToken");
-            window.location.reload();
-          }}
-        >
-          로그아웃
-        </button>
-      ) : (
-        <button className="" onClick={() => navigate("/admin/login")}>
-          로그인
-        </button>
-      )}
-    </div>
+    <>
+      <div
+        className="flex justify-between w-full px-10 py-5"
+        onClick={() => setModalOpen(true)}
+      >
+        <img src={HamburgerIcon} alt="hamburger" className="w-10 cursor-pointer" />
+        {token ? (
+          <button
+            className=""
+            onClick={() => {
+              localStorage.removeItem("accessToken");
+              localStorage.removeItem("refreshToken");
+              window.location.reload();
+            }}
+          >
+            로그아웃
+          </button>
+        ) : (
+          <button className="" onClick={() => navigate("/admin/login")}>
+            로그인
+          </button>
+        )}
+      </div>
+      {modalOpen && <HeaderModal onClose={() => setModalOpen(false)} />}
+    </>
   );
 };
 
