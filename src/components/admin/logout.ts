150a6bc13e2f731f7c737ddeb450896a80bdb584
o2rynn <flsgp0317@gmail.com>
2023-12-12 23:01:13 +0900

    feat: 세션 만료시 로그인 페이지로 이동

--- src/components/admin/logout.ts
+++ src/components/admin/logout.ts
@@ -6,7 +6,7 @@ const logout = () => {
 
   // 로그인 페이지로 리디렉션
   window.alert("세션 만료로 로그아웃 되었습니다. 다시 로그인해주세요.");
-  // window.location.href = "/admin/login";
+  window.location.href = "/admin/login";
 };
 
 export default logout;
